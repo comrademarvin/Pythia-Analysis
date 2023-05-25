07ae485cdb6c20a29c92d6e3e122508bf19fe839
comrademarvin <stephan.stefie@gmail.com>
2023-05-25 15:22:59 +0200

    Investigating threading, added ptHat bins

--- mymain07.cc
+++ mymain07.cc
@@ -16,18 +16,20 @@ void* handle(void* ptr) {
 
     TFile* outFile = new TFile(Form("mymain07_%d.root", ith), "RECREATE");
 
-    TTree* tree = new TTree("tree", "tree");
+    TTree* tree = new TTree(Form("tree_%d", ith), "tree");
 
     double pTHat;
 
     tree->Branch("pTHat", &pTHat, "pTHat/D");
 
-    int N_events = 1000;
+    int N_events = 100;
 
     Pythia pythia;
     pythia.readString("HardQCD:all = on");
     pythia.readString("Beams:eCM = 5020.");
     pythia.readString("Tune:pp = 14");
+    pythia.settings.parm("PhaseSpace:pTHatMin", (ith+1)*16.0);
+    pythia.settings.parm("PhaseSpace:pTHatMax", (ith+2)*16.0);
     pythia.init();
 
     //TH1F *pTHard = new TH1F(Form("pt_hard_%d", ith),"", 10, 0.0, 20.0);
@@ -50,6 +52,9 @@ void* handle(void* ptr) {
 
     outFile->Write();
     outFile->Close();
+
+    delete tree;
+    delete outFile;
 }
 
 int main() {
@@ -65,9 +70,28 @@ int main() {
         th[i]->Run();
     }
 
+    // while (true) {
+    //     bool running = false;
+    //     for (int i = 0; i < nBins; i++) {
+    //         if (th[i]->GetState() == 2) {
+    //             cout << "RUNNING\n";
+    //             running = true;
+    //         }
+    //     }
+
+    //     if (!running) break;
+    //     cout << "REPEATING\n";
+    // }
+
     for (int i = 0; i < nBins; i++) {
         th[i]->Join();
     }
+
+    // for (int i = 0; i < nBins; i++) {
+    //     delete th[i];
+    // }
+
+    // delete[] th;
 
     return 0;
 }
