63c34c26551068e08f482b7970094a92c08a2e75
comrademarvin <stephan.stefie@gmail.com>
2023-07-27 20:12:35 +0200

    Forced D Meson decay run 500k

--- mymain09.cc
+++ mymain09.cc
@@ -47,7 +47,7 @@ int main() {
     }
 
     // Number of events to generate per bin.
-    int N_events = 2000000;
+    int N_events = 500000;
 
     // Store multiplicity of each event
     int multBins;
@@ -73,8 +73,8 @@ int main() {
 
         pythia.readString("Beams:eCM = 5020.");
         pythia.readString("Tune:pp = 14");
-        // pythia.readString("411:onMode=off");
-        // pythia.readString("411:onIfAny=13");
+        pythia.readString("411:onMode=off");
+        pythia.readString("411:onIfAny=13");
         pythia.settings.parm("PhaseSpace:pTHatMin", binEdges[iBin]);
         pythia.settings.parm("PhaseSpace:pTHatMax", binEdges[iBin + 1]);
         pythia.init();
